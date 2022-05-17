6f1f428f1bdb8d9e3488e338d1ae4d46b5250020
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-17 02:55:54 -0400

    Refactoring

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
   const [light, setLight] = useState('off');
   const dark = (light === 'off') ? 'dark' : '';
-  
+  const switchLight = () => setLight(light => light === "on" ? "off" : "on");
+
   return (
     <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton light={light} setLight={setLight}/>
+        <LightSwitchButton light={light} setLight={setLight}  switchLight={switchLight}/>
         <TextRepeaterButton />
       </section>     
     </div>
